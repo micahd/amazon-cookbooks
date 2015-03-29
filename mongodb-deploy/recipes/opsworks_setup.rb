80dc7ae41a116585dd0fcfb26ce0488d5369d8d5
Micah Davidson <micah@mond.us>
2015-03-29 14:12:20 -0600

    Another stab at having the mongodb configuration proper on startup.

--- mongodb-deploy/recipes/opsworks_setup.rb
+++ mongodb-deploy/recipes/opsworks_setup.rb
@@ -1,4 +1,4 @@
-node.default['mongodb']['dbpath'] = "/data/db"
-node.normal['mongodb']['dbpath'] = "/data/db"
+node.normal['mongodb']['data_dir'] = '/data/mongodb/db'
+node.normal['mongodb']['log_dir'] = '/data/mongodb/log'
 include_recipe "mongodb-10gen::default"
 
