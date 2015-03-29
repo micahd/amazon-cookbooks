ce0aa1a779b652cd93ba0613ebb0cdcf2192c6b6
Micah Davidson <micah@mond.us>
2015-03-29 11:15:32 -0600

    Another stab at replacing the default configuration for dbpath to be proper.

--- mongodb-deploy/recipes/opsworks_setup.rb
+++ mongodb-deploy/recipes/opsworks_setup.rb
@@ -1,3 +1,4 @@
-node.normal['mongodb']['data_dir'] = "/data/db"
+node.default['mongodb']['dbpath'] = "/data/db"
+node.normal['mongodb']['dbpath'] = "/data/db"
 include_recipe "mongodb-10gen::default"
 
